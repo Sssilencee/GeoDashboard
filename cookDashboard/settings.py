233ca928c8562524b3198cfe3de954026ce8185a
Matvey Svetlov <80686174+Sssilencee@users.noreply.github.com>
2022-08-17 18:47:11 +0300

    Update settings.py

--- cookDashboard/settings.py
+++ cookDashboard/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x0wm)4&)ym5+*%5xrbh0i=k8#q!!kv0iuh&zso7r055$5@zsgo'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
